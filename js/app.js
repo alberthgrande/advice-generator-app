91e096232cc4d2da42408056b74f12aca9c9433b
Alberth G. Ruado <alberthruado@gmail.com>
2023-01-29 12:38:15 +0800

    Update Added functionality Challenge done

--- js/app.js
+++ js/app.js
@@ -1,3 +1,19 @@
-fetch("https://api.adviceslip.com/advice")
-  .then((res) => res.json())
-  .then((data) => console.log(data));
+const adviceID = document.getElementById("advice-id");
+const adviceText = document.getElementById("advice-text");
+const adviceBtn = document.getElementById("generate-btn");
+const url = "https://api.adviceslip.com/advice";
+
+// fectch data using async method
+async function getAdvice() {
+  // fetch data using fetch method
+  const res = await fetch(url);
+  const {
+    slip: { id, advice },
+  } = await res.json();
+  adviceID.innerText = id;
+  adviceText.innerText = advice;
+}
+
+adviceBtn.addEventListener("click", function () {
+  getAdvice();
+});
